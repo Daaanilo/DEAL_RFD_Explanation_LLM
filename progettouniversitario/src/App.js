594fd372dadb44abc5c9ac06a82e4f03a1f07129
Daaanilo <145839755+Daaanilo@users.noreply.github.com>
2024-04-23 21:07:25 +0200

    Add files via upload

--- progettouniversitario/src/App.js
+++ progettouniversitario/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Stitch, RemoteMongoClient, BSON } from 'mongodb-stitch-browser-sdk';
+import axios from "axios"
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [file, setFile] = useState("");
+  
+  const submitImage=async(e)=>{
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("title", title);
+    formData.append("file", file);
+    
+    console.log(title, file)
+    const result = await axios.post("http://localhost:5000/upload-files", formData,
+    {
+      headers: {"Content-Type": "multipart/form-data"},
+    });
+
+    console.log(result);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="formStyle" onSubmit={submitImage}>
+        <h4>Upload JSON</h4>
+        <br/>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Title"
+          required
+          onChange={(e)=>setTitle(e.target.value)}/>
+          <input
+            type="file"
+            class="form-control"
+            accept="application/pdf"
+            required
+            onChange={(e)=>setFile(e.target.files[0])}
+            />
+ 
+      <button class="btn btn-primary" type="submit">
+        Carica
+      </button>     </form>
     </div>
   );
 }
 
 export default App;
+
