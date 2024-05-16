3a6b83d89ecfe7b1f6afd8148273c2a57ccfc570
Daaanilo <d.gisolfi@studenti.unisa.it>
2024-05-16 18:25:11 +0200

    Modifiche estetiche

--- src/FileDetailsPage.js
+++ src/FileDetailsPage.js
@@ -11,28 +11,80 @@ const { handleUserInput } = require('./chatgptapi.js');
 
 const FileDetailsPage = ({ fileName, onBack }) => {
   const [fileContent, setFileContent] = useState([]);
-  const [rowsToRemove, setRowsToRemove] = useState([]);
   const [selectedRows, setSelectedRows] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [isTextGenerated, setIsTextGenerated] = useState(false);
-  const [responseAI, setResponseAI] = useState(""); // Stato per salvare la risposta generata dall'IA
+  const [responseAI, setResponseAI] = useState("");
+  const [allRFDs, setAllRFDs] = useState([]);
+
+  const info = {
+    name: [],
+    header: [],
+    size: [],
+    format: [],
+    col_number: [],
+    row_number: [],
+    separator: [],
+    blank_char: [],
+
+    os: [],
+    os_version: [],
+    processor: [],
+    thread: [],
+    core: [],
+    ram: [],
+    
+    time_limit: [],
+    memory_limit: [],
+    general_error: []
+  };
+
+  const header = [];
 
   useEffect(() => {
     axios.get(`http://localhost:5000/files/${fileName}`)
       .then(response => {
         const data = response.data;
-        const formattedData = Object.entries(data).map(([key, value]) => ({ key, value }));
-        setFileContent(formattedData);
+        if (data) {
+          const formattedData = Object.entries(data).map(([key, value]) => ({ key, value }));
+          setFileContent(formattedData);
+          extractLhsAndRhs(data);
+        } else {
+          console.error('Dati non definiti o nulli');
+        }
       })
       .catch(error => {
         console.error(error);
         alert('Errore durante il recupero del contenuto del file');
       });
   }, [fileName]);
+  
 
-  const removeRow = (keyToRemove, valueToRemove) => {
-    setRowsToRemove([...rowsToRemove, { key: keyToRemove, value: valueToRemove }]);
+  const extractLhsAndRhs = (data) => {
+    const extractedRFDs = [];
+    
+    if (data && data.length) {
+      data.forEach(item => {
+        if (item.execution && item.execution.result && item.execution.result.length) {
+          item.execution.result.forEach(execution => {
+            if (execution.data && execution.data.length) {
+              execution.data.forEach(resultData => {
+                if (resultData.lhs && resultData.rhs) {
+                  const lhsColumns = resultData.lhs.map(lhsItem => `${lhsItem.column}@${lhsItem.comparison_relaxation.toFixed(1)}`).join(' ');
+                  const rhsColumns = resultData.rhs.map(rhsItem => `${rhsItem.column}@${rhsItem.comparison_relaxation.toFixed(1)}`).join(' ');
+                  const rfdString = `${lhsColumns} -> ${rhsColumns}`;
+                  extractedRFDs.push(rfdString);
+                }
+              });
+            }
+          });
+        }
+      });
+    } 
+    
+    setAllRFDs(extractedRFDs);
   };
+  
 
   const toggleRowSelection = (index) => {
     const selectedIndex = selectedRows.indexOf(index);
@@ -43,155 +95,105 @@ const FileDetailsPage = ({ fileName, onBack }) => {
     }
   };
 
+  const toggleSelectAll = () => {
+    if (selectedRows.length === allRFDs.length) {
+      setSelectedRows([]);
+    } else {
+      const allIndexes = allRFDs.map((_, index) => index);
+      setSelectedRows(allIndexes);
+    }
+  };
+
   const scrollToBottom = async () => {
-    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
     if (selectedRows.length === 0) {
       alert('Selezionare una o più RFDs');
     } else {
       if (window.confirm('Sei sicuro di voler generare il testo?')) {
+        window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+
         setIsLoading(true);
-        const selectedCC = selectedRows.map(index => cc[index]);
-        const response = await handleUserInput("ciao"); // Chiamata alla funzione che gestisce l'input utente
-        setResponseAI(response); // Salvare la risposta generata dall'IA nello stato locale
+        const selectedRFDs = selectedRows.map(index => allRFDs[index]);
+        //alert(selectedRFDs)
+        const response = await handleUserInput("ciao");
+        setResponseAI(response);
         setIsTextGenerated(true);
         setIsLoading(false);
       }
     }
   };
-  
 
-  
-  // INFO DATASET
-  let name = [];
-  let size = [];
-  let format = [];
-  let col_number = [];
-  let row_number = [];
-
-  // EXECUTION INFO
-  let os = [];
-  let os_version = [];
-  let processor = [];
-  let thread = [];
-  let core = [];
-  let ram = [];
-
-  // ERROR
-  let time_limit = [];
-  let memory_limit = [];
-  let general_error = [];
-
-  let cc = [];
 
   const formatData = (obj, depth = 0) => {
     return Object.entries(obj).map(([key, value]) => {
       const indent = '  '.repeat(depth);
-
-      if (typeof value === 'object') {
-        return (
-          <div key={`${key}-${depth}`} className="nested">
-            <span className="key">{indent}{key}: </span>
-            <div className="value">{formatData(value, depth + 1)}</div>
-          </div>
-        );
-      } else if (!rowsToRemove.find(row => row.key === key && row.value === value)) {
-        switch (key) {
-          case 'name':
-            name.push(value);
-            break;
-          case 'size':
-            size.push(value);
-            break;
-          case 'format':
-            format.push(value);
-            break;
-          case 'col_number':
-            col_number.push(value);
-            break;
-          case 'row_number':
-            row_number.push(value);
-            break;
-          case 'cc':
-            cc.push(value);
-            break;
-          case 'os':
-            os.push(value);
-            break;
-          case 'os_version':
-            os_version.push(value);
-            break;
-          case 'processor':
-            processor.push(value);
-            break;
-          case 'thread':
-            thread.push(value);
-            break;
-          case 'core':
-            core.push(value);
-            break;
-          case 'ram':
-            ram.push(value);
-            break;
-          case 'time_limit':
-            time_limit.push(value);
-            break;
-          case 'memory_limit':
-            memory_limit.push(value);
-            break;
-          case 'general_error':
-            general_error.push(value);
-            break;
-        }
-
-        return (
-          <div key={`${key}-${depth}`} className="card">
-            <div className="card-content">
-              <span className="key">{indent}{key}: </span>
-              <div className="value">
-                <button className="remove-btn" onClick={() => removeRow(key, value)}>❌</button>
-                <div className="cell">{value}</div>
-              </div>
-            </div>
-          </div>
-        );
+      if (key === 'header') {
+        header.push(value);
       }
-
+  
+      if (typeof value === 'object' && value !== null) {
+        return `${indent}${key}: ${formatData(value, depth + 1)}`;
+      }
+      if (info.hasOwnProperty(key)) { 
+        const uniqueValues = new Set(info[key]);
+        if (!uniqueValues.has(value)) {
+          info[key].push(value);
+        }
+      }
       return null;
     });
   };
+  const [selectedHeaderValues, setSelectedHeaderValues] = useState([]);
 
+  const toggleHeaderSelection = (value) => {
+    const selectedIndex = selectedHeaderValues.indexOf(value);
+    if (selectedIndex === -1) {
+      setSelectedHeaderValues([...selectedHeaderValues, value]);
+    } else {
+      setSelectedHeaderValues(selectedHeaderValues.filter(item => item !== value));
+    }
+  };
   fileContent.forEach(row => formatData(row));
 
   return (
     <div className="file-details">
       <h2 className="title">Dettagli del File: {fileName}</h2>
       <button className="back-btn" onClick={onBack}>Torna Indietro</button>
-
+      
+      <div className="container">
+  <div className="card mb-3">
+    <div className="card-header">INFO DATASET <DatabaseIcon /></div>
+    <div className="card-body">
       <div className="container">
         <div className="card mb-3">
-          <div className="card-header">INFO DATASET <DatabaseIcon /></div>
-          <div className="card-body">
-            <div className="container">
-              <div className="card mb-3">
-                <div className="card-header">Header</div>
-                <div className="card-body d-flex flex-row">
-                  {name.map((item, index) => (
-                    <div className="card m-1" key={index}>
-                      <div className="card-body">{item}</div>
-                    </div>
-                  ))}
+          <div className="card-header">Header</div>
+                  {header && header[0] && (
+          <div className="card-body d-flex flex-row flex-wrap">
+            {header[0].map((item, index) => (
+              <div key={index} className="card mb-3">
+                <div className="card-header d-flex justify-content-between align-items-center">
+                  <span>{item}</span>
+                  <input
+                    type="checkbox"
+                    checked={selectedHeaderValues.includes(item)}
+                    onChange={() => toggleHeaderSelection(item)}
+                  />
                 </div>
               </div>
+            ))}
+          </div>
+        )}
+        </div>
 
               <div className="row">
                 <div className="col-md-6">
                   <div className="card mb-3">
                     <div className="card-header">Size & Format <AspectRatioIcon /></div>
                     <div className="card-body">
-                      {size.map((item, index) => (
+                      {info.size.map((item, index) => (
                         <div key={index}>
                           <strong>Size:</strong> {item} <br />
-                          <strong>Format:</strong> {format[index]}
+                          <strong>Format:</strong> {info.format[index]} <br />
+                          <strong>Separator:</strong> {info.separator[index]}
                           <hr />
                         </div>
                       ))}
@@ -203,95 +205,115 @@ const FileDetailsPage = ({ fileName, onBack }) => {
                   <div className="card mb-3">
                     <div className="card-header">Column & Row Number <ColumnsIcon /></div>
                     <div className="card-body">
-                      {col_number.map((item, index) => (
-                        <div key={index}>
-                          <strong>Column:</strong> {item} <br />
-                          <strong>Row:</strong> {row_number[index]}
-                          <hr />
-                        </div>
-                      ))}
-                    </div>
+                      {info.col_number.map((item, index) => (
+                    <div key={index}>
+                    <strong>Column:</strong> {item} <br />
+                    <strong>Row:</strong> {info.row_number[index]} <br />
+                    <strong>Blank char:</strong> {info.blank_char[index]}
+                    <hr />
                   </div>
+                  ))}
                 </div>
-              </div>
+                </div>
             </div>
           </div>
         </div>
-      </div>
+          </div>
+            </div>
+              </div>
 
-      <div className="container">
-        <div className="card mb-3">
-          <div className="card-header">EXECUTION INFO <PcDisplayIcon /></div>
-          <div className="card-body">
-            <div className="row">
-              <div className="col-md-12">
+              
+                <div className="container">
+                  <div className="card mb-3">
+                    <div className="card-header">EXECUTION INFO <PcDisplayIcon /></div>
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col-md-12">
+                          <div className="card-body">
+                            {info.os.map((item, index) => (
+                              <div key={index}>
+                                <strong>Os:</strong> {item} <br />
+                                <strong>Os Version:</strong> {info.os_version[index]} <br />
+                                <strong>Processor:</strong> {info.processor[index]} <br />
+                                <strong>Thread:</strong> {info.thread[index]} <br />
+                                <strong>Core:</strong> {info.core[index]} <br />
+                                <strong>Ram:</strong> {info.ram[index]} <br />
+                                <hr />
+                              </div>
+                            ))}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+            <div className="container">
+              <div className="card mb-3">
+                <div className="card-header">RFDs</div>
                 <div className="card-body">
-                  {os.map((item, index) => (
+                  <button onClick={toggleSelectAll}>
+                    {selectedRows.length === allRFDs.length ? "Deseleziona tutte" : "Seleziona tutte"}
+                  </button>
+                  <ul style={{ whiteSpace: 'pre-wrap' }}>
+                    {allRFDs.map((rfd, index) => {
+                      const containsSelectedHeader = selectedHeaderValues.some(value => rfd.includes(value));
+                      if (containsSelectedHeader) {
+                        return null;
+                      }
+                      return (
+                        <li key={index}>
+                          <input
+                            type="checkbox"
+                            checked={selectedRows.includes(index)}
+                            onChange={() => toggleRowSelection(index)}
+                          />
+                          {rfd}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="container">
+              <div className="card mb-3">
+                <div className="card-header">ERROR <BugIcon /></div>
+                <div className="card-body">
+                  {info.time_limit.map((item, index) => (
                     <div key={index}>
-                      <strong>Os:</strong> {item} <br />
-                      <strong>Os Version:</strong> {os_version[index]} <br />
-                      <strong>Processor:</strong> {processor[index]} <br />
-                      <strong>Thread:</strong> {thread[index]} <br />
-                      <strong>Core:</strong> {core[index]} <br />
-                      <strong>Ram:</strong> {ram[index]} <br />
+                      <strong>Time Limit:</strong> {item} <br />
+                      <strong>Memory Limit:</strong> {info.memory_limit[index]} <br />
+                      <strong>General Error:</strong> {info.general_error[index]} <br />
                       <hr />
                     </div>
                   ))}
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </div>
 
-      <div className="card">
-        <div className="card-header">RFDs</div>
-        <div className="card-body">
-          {cc.map((item, index) => (
-            <div key={index}>
-              <label>
-                <input
-                  type="checkbox"
-                  checked={selectedRows.includes(index)}
-                  onChange={() => toggleRowSelection(index)}
-                />
-                {item}
-              </label>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      <div className="card">
-        <div className="card-header">ERROR <BugIcon /></div>
-          <div className="card-body">
-            {time_limit.map((item, index) => (
-              <div key={index}>
-                <strong>Time Limit:</strong> {item} <br />
-                <strong>Memory Limit:</strong> {memory_limit[index]} <br />
-                <strong>General Error:</strong> {general_error[index]} <br />
-                <hr />
+            <div className="container">
+              <div className="card mb-3">
+                <div className="card-header">TESTO GENERATO <RobotIcon /></div>
+                <div className="card-body">
+                  {isTextGenerated && (
+                    <p>{responseAI}</p>
+                  )}
+                </div>
               </div>
-            ))}
-        </div>
-      </div>
+            </div>
 
-      <div className="card">
-        <div className="card-header">TESTO GENERATO <RobotIcon /></div>
-        <div className="card-body">
-          {isTextGenerated && (
-            <p>{responseAI}</p>
-          )}
-        </div>
-      </div>
+            <div style={{ height: '100px' }}></div>
 
-      <div className="fixed-button-container">
-        <button className="fixed-button" onClick={scrollToBottom}>
-          {isLoading ? "Caricamento..." : "Genera testo"}
-        </button>
-      </div>
-    </div>
-  );
-};
+            <div className="fixed-button-container">
+              <button className="fixed-button" onClick={scrollToBottom}>
+                {isLoading ? "Caricamento..." : "Genera testo"}
+              </button>
+            </div>
+          </div>
+        );
+      };
 
-export default FileDetailsPage;
+      export default FileDetailsPage;
+
