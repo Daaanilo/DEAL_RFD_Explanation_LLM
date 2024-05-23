477735c62bea9153390c53f889f94cbe74ce0fb6
Daaanilo <d.gisolfi@studenti.unisa.it>
2024-05-23 12:16:07 +0200

    Altre modifiche

--- src/FileDetailsPage.js
+++ src/FileDetailsPage.js
@@ -5,6 +5,12 @@ import { ReactComponent as ColumnsIcon } from 'bootstrap-icons/icons/columns.svg
 import { ReactComponent as PcDisplayIcon } from 'bootstrap-icons/icons/pc-display.svg';
 import { ReactComponent as BugIcon } from 'bootstrap-icons/icons/bug.svg';
 import { ReactComponent as RobotIcon } from 'bootstrap-icons/icons/robot.svg';
+import { ReactComponent as Graph } from 'bootstrap-icons/icons/diagram-2.svg';
+import { ReactComponent as ToggleOffIcon } from 'bootstrap-icons/icons/toggle-off.svg';
+import { ReactComponent as ToggleOnIcon } from 'bootstrap-icons/icons/toggle-on.svg';
+import { ReactComponent as PressedIcon } from 'bootstrap-icons/icons/check-square-fill.svg';
+import { ReactComponent as NotPressedIcon } from 'bootstrap-icons/icons/square.svg';
+
 import axios from 'axios';
 import './FileDetailsPage.css';
 const { handleUserInput } = require('./chatgptapi.js');
@@ -16,6 +22,8 @@ const FileDetailsPage = ({ fileName, onBack }) => {
   const [isTextGenerated, setIsTextGenerated] = useState(false);
   const [responseAI, setResponseAI] = useState("");
   const [allRFDs, setAllRFDs] = useState([]);
+  const [cardVisibility, setCardVisibility] = useState({ infoDataset: true, sizeAndFormat: true, columnAndRowNumber: true, executionInfo: true, graphs: true, rfd: true, error: true, generatedText: true });
+
 
   const info = {
     name: [],
@@ -60,31 +68,27 @@ const FileDetailsPage = ({ fileName, onBack }) => {
   }, [fileName]);
   
 
-  const extractLhsAndRhs = (data) => {
-    const extractedRFDs = [];
-    
-    if (data && data.length) {
-      data.forEach(item => {
-        if (item.execution && item.execution.result && item.execution.result.length) {
-          item.execution.result.forEach(execution => {
-            if (execution.data && execution.data.length) {
-              execution.data.forEach(resultData => {
-                if (resultData.lhs && resultData.rhs) {
-                  const lhsColumns = resultData.lhs.map(lhsItem => `${lhsItem.column}@${lhsItem.comparison_relaxation.toFixed(1)}`).join(' ');
-                  const rhsColumns = resultData.rhs.map(rhsItem => `${rhsItem.column}@${rhsItem.comparison_relaxation.toFixed(1)}`).join(' ');
-                  const rfdString = `${lhsColumns} -> ${rhsColumns}`;
-                  extractedRFDs.push(rfdString);
-                }
-              });
-            }
-          });
-        }
-      });
-    } 
-    
-    setAllRFDs(extractedRFDs);
-  };
-  
+const extractLhsAndRhs = (data) => {
+  const extractedRFDs = [];
+
+  if (data && data.length) {
+    data.forEach(item => {
+      if (item.execution && item.execution.result && item.execution.result.data && item.execution.result.data.length) {
+        item.execution.result.data.forEach(resultData => {
+          if (resultData.lhs && resultData.rhs) {
+            const lhsColumns = resultData.lhs.map(lhsItem => `${lhsItem.column}@${lhsItem.comparison_relaxation.toFixed(1)}`).join(' ');
+            const rhsColumns = resultData.rhs.map(rhsItem => `${rhsItem.column}@${rhsItem.comparison_relaxation.toFixed(1)}`).join(' ');
+            const rfdString = `${lhsColumns} -> ${rhsColumns}`;
+            extractedRFDs.push(rfdString);
+          }
+        });
+      }
+    });
+  } 
+
+  setAllRFDs(extractedRFDs);
+};
+
 
   const toggleRowSelection = (index) => {
     const selectedIndex = selectedRows.indexOf(index);
@@ -96,11 +100,14 @@ const FileDetailsPage = ({ fileName, onBack }) => {
   };
 
   const toggleSelectAll = () => {
-    if (selectedRows.length === allRFDs.length) {
+    const visibleRFDsIndexes = allRFDs
+      .map((_, index) => index)
+      .filter(index => !selectedHeaderValues.some(value => allRFDs[index].includes(value)));
+  
+    if (selectedRows.length === visibleRFDsIndexes.length) {
       setSelectedRows([]);
     } else {
-      const allIndexes = allRFDs.map((_, index) => index);
-      setSelectedRows(allIndexes);
+      setSelectedRows(visibleRFDsIndexes);
     }
   };
 
@@ -113,7 +120,7 @@ const FileDetailsPage = ({ fileName, onBack }) => {
 
         setIsLoading(true);
         const selectedRFDs = selectedRows.map(index => allRFDs[index]);
-        //alert(selectedRFDs)
+        alert(selectedRFDs)
         const response = await handleUserInput(
           "Potresti spiegarmi il significato delle seguenti dipendenze RFD?"
         + "Vorrei una comprensione approfondita delle variabili coinvolte e delle relative soglie di tolleranza. Per esempio, nel seguente elenco di dipendenze:"
@@ -159,6 +166,14 @@ const FileDetailsPage = ({ fileName, onBack }) => {
   };
   fileContent.forEach(row => formatData(row));
 
+
+  const toggleCardVisibility = (cardName) => {
+    setCardVisibility({
+      ...cardVisibility,
+      [cardName]: !cardVisibility[cardName]
+    });
+  };
+
   return (
     <div className="file-details">
       <h2 className="title">Dettagli del File: {fileName}</h2>
@@ -166,159 +181,193 @@ const FileDetailsPage = ({ fileName, onBack }) => {
       
       <div className="container">
   <div className="card mb-3">
-    <div className="card-header">INFO DATASET <DatabaseIcon /></div>
-    <div className="card-body">
-      <div className="container">
-        <div className="card mb-3">
-          <div className="card-header">Header</div>
-                  {header && header[0] && (
-          <div className="card-body d-flex flex-row flex-wrap">
-            {header[0].map((item, index) => (
-              <div key={index} className="card mb-3">
-                <div className="card-header d-flex justify-content-between align-items-center">
-                  <span>{item}</span>
-                  <input
-                    type="checkbox"
-                    checked={selectedHeaderValues.includes(item)}
-                    onChange={() => toggleHeaderSelection(item)}
-                  />
-                </div>
-              </div>
-            ))}
-          </div>
+    <div className="d-flex justify-content-between align-items-center card-header">
+      <span>INFO DATASET <DatabaseIcon/></span>
+      <div>
+        {cardVisibility.infoDataset ? (
+          <ToggleOnIcon onClick={() => toggleCardVisibility('infoDataset')} />
+        ) : (
+          <ToggleOffIcon onClick={() => toggleCardVisibility('infoDataset')} />
         )}
-        </div>
-
-              <div className="row">
-                <div className="col-md-6">
-                  <div className="card mb-3">
-                    <div className="card-header">Size & Format <AspectRatioIcon /></div>
-                    <div className="card-body">
-                      {info.size.map((item, index) => (
-                        <div key={index}>
-                          <strong>Size:</strong> {item} <br />
-                          <strong>Format:</strong> {info.format[index]} <br />
-                          <strong>Separator:</strong> {info.separator[index]}
-                          <hr />
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-md-6">
-                  <div className="card mb-3">
-                    <div className="card-header">Column & Row Number <ColumnsIcon /></div>
-                    <div className="card-body">
-                      {info.col_number.map((item, index) => (
-                    <div key={index}>
-                    <strong>Column:</strong> {item} <br />
-                    <strong>Row:</strong> {info.row_number[index]} <br />
-                    <strong>Blank char:</strong> {info.blank_char[index]}
-                    <hr />
-                  </div>
-                  ))}
-                </div>
-                </div>
-            </div>
-          </div>
-        </div>
-          </div>
-            </div>
-              </div>
-
-              
-                <div className="container">
-                  <div className="card mb-3">
-                    <div className="card-header">EXECUTION INFO <PcDisplayIcon /></div>
-                    <div className="card-body">
-                      <div className="row">
-                        <div className="col-md-12">
-                          <div className="card-body">
-                            {info.os.map((item, index) => (
-                              <div key={index}>
-                                <strong>Os:</strong> {item} <br />
-                                <strong>Os Version:</strong> {info.os_version[index]} <br />
-                                <strong>Processor:</strong> {info.processor[index]} <br />
-                                <strong>Thread:</strong> {info.thread[index]} <br />
-                                <strong>Core:</strong> {info.core[index]} <br />
-                                <strong>Ram:</strong> {info.ram[index]} <br />
-                                <hr />
-                              </div>
-                            ))}
-                          </div>
+      </div>
+    </div>
+    {cardVisibility.infoDataset && (
+      <div className="card-body">
+        <div className="container">
+          <div className="card mb-3">
+            <div className="card-header">Header</div>
+            {header && header[0] && (
+                <div className="card-body d-flex flex-row flex-wrap">
+                  {header[0].map((item, index) => (
+                    <div key={index} className="card mb-3">
+                      <div className="card-header d-flex justify-content-between align-items-center">
+                        <span>{item}</span>
+                        <div>
+                          {selectedHeaderValues.includes(item) ? (
+                            <NotPressedIcon onClick={() => toggleHeaderSelection(item)} />
+                          ) : (
+                            <PressedIcon onClick={() => toggleHeaderSelection(item)} />
+                          )}
                         </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-
-            <div className="container">
-              <div className="card mb-3">
-                <div className="card-header">RFDs</div>
-                <div className="card-body">
-                  <button onClick={toggleSelectAll}>
-                    {selectedRows.length === allRFDs.length ? "Deseleziona tutte" : "Seleziona tutte"}
-                  </button>
-                  <ul style={{ whiteSpace: 'pre-wrap' }}>
-                    {allRFDs.map((rfd, index) => {
-                      const containsSelectedHeader = selectedHeaderValues.some(value => rfd.includes(value));
-                      if (containsSelectedHeader) {
-                        return null;
-                      }
-                      return (
-                        <li key={index}>
-                          <input
-                            type="checkbox"
-                            checked={selectedRows.includes(index)}
-                            onChange={() => toggleRowSelection(index)}
-                          />
-                          {rfd}
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
-              </div>
-            </div>
-
-            <div className="container">
-              <div className="card mb-3">
-                <div className="card-header">ERROR <BugIcon /></div>
-                <div className="card-body">
-                  {info.time_limit.map((item, index) => (
-                    <div key={index}>
-                      <strong>Time Limit:</strong> {item} <br />
-                      <strong>Memory Limit:</strong> {info.memory_limit[index]} <br />
-                      <strong>General Error:</strong> {info.general_error[index]} <br />
-                      <hr />
-                    </div>
                   ))}
                 </div>
-              </div>
-            </div>
+              )}
+          </div>
 
-            <div className="container">
+          <div className="row">
+            <div className="col-md-6">
               <div className="card mb-3">
-                <div className="card-header">TESTO GENERATO <RobotIcon /></div>
-                <div className="card-body">
-                  {isTextGenerated && (
-                    <p>{responseAI}</p>
-                  )}
-                </div>
+                <div className="card-header">Size & Format {cardVisibility.sizeAndFormat ? <ToggleOnIcon onClick={() => toggleCardVisibility('sizeAndFormat')} /> : <ToggleOffIcon onClick={() => toggleCardVisibility('sizeAndFormat')} />} <AspectRatioIcon /></div>
+                {cardVisibility.sizeAndFormat && (
+                  <div className="card-body">
+                    {info.size.map((item, index) => (
+                      <div key={index}>
+                        <strong>Size:</strong> {item} <br />
+                        <strong>Format:</strong> {info.format[index]} <br />
+                        <strong>Separator:</strong> {info.separator[index]}
+                        <hr />
+                      </div>
+                    ))}
+                  </div>
+                )}
               </div>
             </div>
 
-            <div style={{ height: '100px' }}></div>
-
-            <div className="fixed-button-container">
-              <button className="fixed-button" onClick={scrollToBottom}>
-                {isLoading ? "Caricamento..." : "Genera testo"}
-              </button>
+            <div className="col-md-6">
+              <div className="card mb-3">
+                <div className="card-header">Column & Row Number {cardVisibility.columnAndRowNumber ? <ToggleOnIcon onClick={() => toggleCardVisibility('columnAndRowNumber')} /> : <ToggleOffIcon onClick={() => toggleCardVisibility('columnAndRowNumber')} />} <ColumnsIcon /></div>
+                {cardVisibility.columnAndRowNumber && (
+                  <div className="card-body">
+                    {info.col_number.map((item, index) => (
+                      <div key={index}>
+                        <strong>Column:</strong> {item} <br />
+                        <strong>Row:</strong> {info.row_number[index]} <br />
+                        <strong>Blank char:</strong> {info.blank_char[index]}
+                        <hr />
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-        );
-      };
+        </div>
+      </div>
+    )}
+  </div>
 
-      export default FileDetailsPage;
+      <div className="card mb-3">
+        <div className="d-flex justify-content-between align-items-center card-header">
+              <span>EXECUTION INFO <PcDisplayIcon/></span>
+              <div>
+                {cardVisibility.executionInfo ? (
+                  <ToggleOnIcon onClick={() => toggleCardVisibility('executionInfo')} />
+                ) : (
+                  <ToggleOffIcon onClick={() => toggleCardVisibility('executionInfo')} />
+                )}
+              </div>
+            </div>
+            {cardVisibility.executionInfo && (
+              <div className="card-body">
+                              {info.os.map((item, index) => (
+                        <div key={index}>
+                          <strong>Os:</strong> {item} <br />
+                          <strong>Os Version:</strong> {info.os_version[index]} <br />
+                          <strong>Processor:</strong> {info.processor[index]} <br />
+                          <strong>Thread:</strong> {info.thread[index]} <br />
+                          <strong>Core:</strong> {info.core[index]} <br />
+                          <strong>Ram:</strong> {info.ram[index]} <br />
+                          <hr />
+                        </div>
+                      ))}
+            </div>
+          )}
+        </div>
 
+        <div className="card mb-3">
+        <div className="d-flex justify-content-between align-items-center card-header">
+          <span>GRAFICI <Graph/> </span>
+           {cardVisibility.graphs ? <ToggleOnIcon onClick={() => toggleCardVisibility('graphs')} /> : <ToggleOffIcon onClick={() => toggleCardVisibility('graphs')} />}</div>
+            {cardVisibility.graphs && (
+              <div className="card-body">
+                TEST
+              </div>
+            )}
+          </div>
+
+
+        <div className="card mb-3">
+        <div className="d-flex justify-content-between align-items-center card-header">
+          <span>RFDs </span>{cardVisibility.rfd ? <ToggleOnIcon onClick={() => toggleCardVisibility('rfd')} /> : <ToggleOffIcon onClick={() => toggleCardVisibility('rfd')} />} </div>
+            {cardVisibility.rfd && (
+              <div className="card-body">
+                <button onClick={toggleSelectAll}>
+                  {selectedRows.length === allRFDs.length ? "Deseleziona tutte" : "Seleziona tutte"}
+                </button>
+                <ul style={{ whiteSpace: 'pre-wrap' }}>
+                  {allRFDs.map((rfd, index) => {
+                    const containsSelectedHeader = selectedHeaderValues.some(value => rfd.includes(value));
+                    if (containsSelectedHeader) {
+                      return null;
+                    }
+                    return (
+                      <li key={index}>
+                        <input
+                          type="checkbox"
+                          checked={selectedRows.includes(index)}
+                          onChange={() => toggleRowSelection(index)}
+                        />
+                        {rfd}
+                      </li>
+                    );
+                  })}
+                </ul>
+              </div>
+            )}
+        </div>
+
+        <div className="card mb-3">
+        <div className="d-flex justify-content-between align-items-center card-header">
+          <span>ERROR <BugIcon/> </span>
+           {cardVisibility.error ? <ToggleOnIcon onClick={() => toggleCardVisibility('error')} /> : <ToggleOffIcon onClick={() => toggleCardVisibility('error')} />}</div>
+            {cardVisibility.error && (
+              <div className="card-body">
+                {info.time_limit.map((item, index) => (
+                  <div key={index}>
+                    <strong>Time Limit:</strong> {item} <br />
+                    <strong>Memory Limit:</strong> {info.memory_limit[index]} <br />
+                    <strong>General Error:</strong> {info.general_error[index]} <br />
+                    <hr />
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+
+          <div className="card mb-3">
+        <div className="d-flex justify-content-between align-items-center card-header">
+              <span>TESTO GENERATO <RobotIcon/></span> {cardVisibility.generatedText ? <ToggleOnIcon onClick={() => toggleCardVisibility('generatedText')} /> : <ToggleOffIcon onClick={() => toggleCardVisibility('generatedText')} />}</div>
+            {cardVisibility.generatedText && (
+              <div className="card-body">
+                {isTextGenerated && (
+                  <p>{responseAI}</p>
+                )}
+              </div>
+            )}
+          </div>
+
+        <div style={{ height: '100px' }}></div>
+
+        <div className="fixed-button-container">
+          <button className="fixed-button" onClick={scrollToBottom}>
+            {isLoading ? "Caricamento..." : "Genera testo"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FileDetailsPage;
